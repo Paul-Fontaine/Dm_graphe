e831ec7c5f5ad34f655ae3114e154ba98cb82f31
Paul Fontaine <fontainepaul36@gmail.com>
2023-12-10 17:05:09 +0100

    city -> town

--- tile.py
+++ tile.py
@@ -4,9 +4,9 @@ from lib_graph.vertex import Vertex
 
 
 class Tile(Vertex):
-    def __init__(self, coord: Tuple[int, int], ground: str, altitude: int, city: bool = False, name: str = "not named tile"):
+    def __init__(self, coord: Tuple[int, int], ground: str, altitude: int, town: bool = False, name: str = "not named tile"):
         Vertex.__init__(self, name)
         self.ground = ground
         self.altitude = altitude
         self.coord = coord
-        self.city = city
+        self.town = town
